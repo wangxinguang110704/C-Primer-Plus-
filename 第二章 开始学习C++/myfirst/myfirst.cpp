c766ac09a9868d009e34ca283aa36be054b8ce9b
wangxinguang110704 <wang.xinguang110704@gmail.com>
2017-04-24 21:37:14 +0800

    添加代码
    增加了前两章的代码，其中第三章还不全

---
+++ 第二章 开始学习C++/myfirst/myfirst.cpp
@@ -0,0 +1,11 @@
+// myfirst.cpp--displays a message
+
+#include <iostream>                            //C++头文件引用可以不加.h
+int main()
+{
+    using namespace std;
+    cout << "come up and C++ me some time.";
+    cout << endl;                              //<<:插入符号；endl:换行
+    cout << "You won't regret it!" << endl;
+    return 0;
+}
