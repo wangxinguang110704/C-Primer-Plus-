88e248dec952f87dd8a287a2439eba40b78e5c77
wangxinguang110704 <wang.xinguang110704@gmail.com>
2017-04-24 22:52:33 +0800

    完善第三章
    好累啊

---
+++ 第三章 处理数据/3.5 chartype/chartype.cpp
@@ -0,0 +1,13 @@
+// chartype.cpp -- the char typed
+#include <iostream>
+int main()
+{
+    using namespace std;
+    char ch;               //declare a char variable
+
+    cout << "Enter a character: " << endl;
+    cin >> ch;
+    cout << "Hella! ";
+    cout << "Thank you for the " << ch << " character." << endl;
+    return 0;
+}
