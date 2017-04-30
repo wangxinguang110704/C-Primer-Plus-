becd45b02b3cf53f25a0ae159b4763114475e842
wangxinguang110704 <wang.xinguang110704@gmail.com>
2017-04-30 10:01:29 +0800

    第五章全
    恒

---
+++ 第五章 循环和关系表达式/5.19 textin4/textin4.cpp
@@ -0,0 +1,17 @@
+// textin4.cpp -- reading chars with cin.get()
+#include <iostream>
+#include <stdio.h>
+int main()
+{
+    using namespace std;
+    int ch;                    // should be int ,not char
+    int count = 0;
+
+    while ((ch = cin.get()) != EOF)     // test for end-of-file
+    {
+        cout.put(char(ch));
+        ++count;
+    }
+    cout << endl << count << " characters read\n";
+    return 0;
+}
